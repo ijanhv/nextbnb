f7ba5b3f4e8e7ad334fd658b7ab4f4a00ea37411
Janhavi Patil <patil.janhavi1207@gmail.com>
2023-08-30 17:42:56 +0530

    initial setup

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter, Nunito } from 'next/font/google'
+import Navbar from './components/navbar/Navbar'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb clone built with Next.js and Tailwind CSS',
+}
+
+const font = Nunito({
+  subsets: ['latin'],
+})
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  )
+}
