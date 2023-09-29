5bc91845631d777809444b19ebdd54eba1fbe6d9
Janhavi Patil <patil.janhavi1207@gmail.com>
2023-09-29 15:03:05 +0530

    add favorites api

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,28 @@
-import ClientOnly from "./components/ClientOnly";
+import { getCurrentUser } from "./actions/getCurrentUser";
+import getListings from "./actions/getListings";
 import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
 
-export default function Home() {
+export default async function Home() {
+  const currentUser = await getCurrentUser();
+  const listings = await getListings();
+
+
+  if (listings.length === 0) return <EmptyState showReset />;
+
   return (
-
-      <Container>
-        <div>Hello Airbnb</div>
-      </Container>
-
+    <Container>
+      HIII
+      <div className="text-black pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        {listings.map((listing: any) => {
+          return (<ListingCard
+           currentUser={currentUser}
+            key={listing.id}
+            data={listing}
+          />);
+        })}
+      </div>
+    </Container>
   );
 }
