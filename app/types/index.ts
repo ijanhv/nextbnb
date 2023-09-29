8aef0aace5eefacd65c8f515cc8abcd91a7bb86f
Janhavi Patil <patil.janhavi1207@gmail.com>
2023-09-29 11:39:08 +0530

    add ts-ignore

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,8 +1,8 @@
 import { User } from "@prisma/client";
 
+//@ts-ignore
 export type SafeUser = Omit<User, "createdAt" | "updatedAt" | "emailVerified"> & {
     createdAt: string;
     updatedAt: string;
     emailVerified: string | null;
-    image: string | null;
 };
